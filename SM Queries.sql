5d491111d7d4b85e371e6df11c936817d8dbbe5c
AravindClgAcc <missings1234@gmail.com>
2023-09-04 19:40:29 +0530

    ffsf

--- SM Queries.sql
+++ SM Queries.sql
@@ -1,9 +1,10 @@
 SELECT * FROM waggle.student_activity_progress_data ORDER BY CreatedDate DESC LIMIT 200 ;
-SELECT COUNT(1) FROM waggle.student_activity_progress_data WHERE CreatedDate >'2023-08-31' AND IsPublished=1 ORDER BY  CreatedDate DESC ;
-SELECT COUNT(1) FROM waggle.student_activity_progress_data WHERE CreatedDate >'2023-08-01' AND IsPublished=0 ORDER BY  CreatedDate DESC  ;
+SELECT COUNT(1) FROM waggle.student_activity_progress_data WHERE CreatedDate >'2023-09-04' AND IsPublished=1 ORDER BY  CreatedDate DESC ;
+SELECT COUNT(1) FROM waggle.student_activity_progress_data WHERE CreatedDate >'2023-08-31' AND IsPublished=0 ORDER BY  CreatedDate DESC  ;
 SELECT COUNT(1) FROM waggle_roster_sync.ed_districts WHERE OrgRefId IS NOT NULL  OR OrgRefId!='' ;
 call waggle_roster_sync.GetSchoolRosterDetails() ;
-SELECT COUNT(1) FROM waggle.student_activity_progress_data WHERE IsPublished=1;
-SELECT  COUNT(DISTINCT StudentRefId) TotalStudents  FROM waggle.student_activity_progress_data WHERE IsPublished=1 ORDER BY  CreatedDate DESC ;
+SELECT COUNT(1) FROM waggle.student_activity_progress_data WHERE IsPublished=1 ;
+SELECT  COUNT(DISTINCT StudentRefId) TotalStudents  FROM waggle.student_activity_progress_data WHERE IsPublished=0 ORDER BY  CreatedDate DESC ;
 SELECT COUNT(1) FROM waggle.student_activity_progress_data WHERE IsPublished=0 ORDER BY  CreatedDate DESC  ;
 SELECT COUNT(1) FROM waggle.student_activity_progress_data WHERE CreatedDate LIKE '%2023-08-29%' AND IsPublished=1 ORDER BY  CreatedDate DESC ;
+SELECT * FROM waggle.student_activity_progress_data WHERE CreatedDate >'2023-09-01' AND IsPublished=0 ORDER BY  CreatedDate DESC LIMIT 1 ;
